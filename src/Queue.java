d5a461e212b1e15b1739f59268744b27b67b8d14
Dhanalaxmi-Musku <135241955+Dhanalaxmi-Musku@users.noreply.github.com>
2024-12-12 22:12:28 +0530

    First commit:Empty Queue

---
+++ src/Queue.java
@@ -0,0 +1,8 @@
+
+public class Queue <T>{
+	 private LinkedList<T> list;
+	    public Queue() {
+	        list = new LinkedList<>();
+	    }
+
+}
---
+++ src/QueueMain.java
@@ -0,0 +1,9 @@
+
+public class QueueMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
